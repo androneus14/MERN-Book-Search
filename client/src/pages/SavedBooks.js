a7a2f4408e0f5e450de54aaf2b2b6a8ad33f1bd7
Andy Nguyen <andynguyen_3@hotmail.com>
2022-07-21 15:13:55 +1000

    making changes to find the bug

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -12,7 +12,7 @@ const SavedBooks = () => {
   const { loading, data } = useQuery(GET_ME);
   const [removeBook, { error }] = useMutation(REMOVE_BOOK);
   // const [userData, setUserData] = useState({});
-  const userData = data?.me || data?.user || {};
+  const userData = data?.me || {};
 
   // use this to determine if `useEffect()` hook needs to run again
   // const userDataLength = Object.keys(userData).length;
